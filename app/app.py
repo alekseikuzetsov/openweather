6302b3ff1c8f83ec5b6db2df9bff9fadf4ebdab1
Aleksei Kuznetsov <aleksei.kuznetsovv@gmail.com>
2020-06-28 21:06:32 +0100

    Added token_required decorator, login and redister functions. Some tests for bad cases had been added.

--- app/app.py
+++ app/app.py
@@ -1,5 +1,10 @@
-from flask import Flask
+import datetime
+
+import flask_bcrypt
+import jwt
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
+from functools import wraps
 
 
 app = Flask(__name__)
@@ -11,13 +16,52 @@ db = cluster['myproject']
 users = db['Users']
 items = db['Items']
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        except:
+            return jsonify({'message': 'UNAUTHORISED USER'})
+
+        return f(*args, **kwargs)
+
+    return decorated
+
 @app.route('/register', methods=['POST'])
 def register():
-    return
+    auth = request.authorization
+    if not auth is None:
+        if auth.username == '':
+            return jsonify({'message': 'USERNAME MUST BE NON-EMPTY'})
+        if auth.password == '':
+            return jsonify({'message': 'PASSWORD MUST BE NON-EMPTY'})
+
+        if users.find_one({'username': auth.username}):
+            return jsonify({'message': 'USERNAME IS IN USE'})
+        else:
+            hashed_password = flask_bcrypt.generate_password_hash(auth.password).decode('utf-8')
+            users.insert_one({'username': auth.username, 'password': hashed_password})
+            return jsonify({'message': 'REGISTERED SUCCESSFULLY'})
+    return jsonify({'message': 'CREDENTIAL DETAILS REQUIRED'})
+
 
 @app.route('/login', methods=['POST'])
 def login():
-    return
+    auth = request.authorization
+    if auth and auth.username and auth.password:
+        user = users.find_one({'username': auth.username})
+        if user and flask_bcrypt.check_password_hash(user['password'], auth.password):
+            token = jwt.encode({'user': auth.username,
+                               'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60)
+                                },
+                               app.config['SECRET_KEY'])
+            return jsonify({'token': token.decode('UTF-8')})
+        else:
+            return jsonify({'message': 'INVALID LOGIN OR PASSWORD'})
+    return jsonify({'message': 'CREDENTIAL DETAILS REQUIRED'})
 
 @app.route('/items/new', methods=['POST'])
 def new_item():
@@ -30,3 +74,6 @@ def get_items():
 @app.route('/items/:id', methods=['DELETE'])
 def delete_item(id):
     return
+
+if __name__ == '__main__':
+    app.run()
--- test.py
+++ test.py
@@ -1,9 +1,65 @@
-from app import app
+from base64 import b64encode
+
+from app.app import app
 import unittest
+import json
+
 
 class FlaskTestCase(unittest.TestCase):
+    # TODO test cases for good scenario
 
     def test_register(self):
         tester = app.test_client(self)
         response = tester.post('/register')
         self.assertEqual(response.status_code, 200)
+
+    def test_register_used_un(self):
+        tester = app.test_client(self)
+        credentials = b64encode(b"bob:bob").decode('utf-8')
+        response = tester.post('/register', headers={"Authorization": f"Basic {credentials}"})
+        json_resp = json.loads(response.data)
+        self.assertEqual(json_resp['message'], 'USERNAME IS IN USE')
+
+    def test_register_no_un(self):
+        tester = app.test_client(self)
+        credentials = b64encode(b":bob").decode('utf-8')
+        response = tester.post('/register', headers={"Authorization": f"Basic {credentials}"})
+        json_resp = json.loads(response.data)
+        self.assertEqual(json_resp['message'], 'USERNAME MUST BE NON-EMPTY')
+
+    def test_register_no_pw(self):
+        tester = app.test_client(self)
+        credentials = b64encode(b"bob:").decode('utf-8')
+        response = tester.post('/register', headers={"Authorization": f"Basic {credentials}"})
+        json_resp = json.loads(response.data)
+        self.assertEqual(json_resp['message'], 'PASSWORD MUST BE NON-EMPTY')
+
+    def test_register_no_cred(self):
+        tester = app.test_client(self)
+        credentials = b64encode(b"").decode('utf-8')
+        response = tester.post('/register', headers={"Authorization": f"Basic {credentials}"})
+        json_resp = json.loads(response.data)
+        self.assertEqual(json_resp['message'], 'CREDENTIAL DETAILS REQUIRED')
+
+    def test_login(self):
+        tester = app.test_client(self)
+        response = tester.post('/login')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_no_cd(self):
+        tester = app.test_client(self)
+        credentials = b64encode(b"").decode('utf-8')
+        response = tester.post('/login', headers={"Authorization": f"Basic {credentials}"})
+        json_resp = json.loads(response.data)
+        self.assertEqual(json_resp['message'], 'CREDENTIAL DETAILS REQUIRED')
+
+    def test_login_no_login(self):
+        tester = app.test_client(self)
+        credentials = b64encode(b"sdsd:sdsdds").decode('utf-8')
+        response = tester.post('/login', headers={"Authorization": f"Basic {credentials}"})
+        json_resp = json.loads(response.data)
+        self.assertEqual(json_resp['message'], 'INVALID LOGIN OR PASSWORD')
+
+
+if __name__ == '__main__':
+    unittest.main()
